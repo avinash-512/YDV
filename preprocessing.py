4fea76e9f31cb409d82ed0f477a451377fa4b5ba
avinah512 <abhinayavinash@iitkgp.ac.in>
2016-10-25 14:28:40 +0530

    YAJUgit add preprocessing.py  FINISH THIS..

--- preprocessing.py
+++ preprocessing.py
@@ -4,3 +4,27 @@
     
         # OUTPUT FORMAT = All text files should have only text not the
         # timestamps!
+
+import os
+
+def list_files(folder):
+    current_path = os.getcwd()
+    print(current_path)
+    folder_path = str(current_path) + str("/") + str(folder) + str("/")
+    list_command = "ls " + folder_path
+    file_list = os.popen(list_command).read().split("\n")
+    file_list.remove('')
+    return file_list
+
+def process(file_list,folder):
+    current_path = os.getcwd()
+    folder_path = str(current_path) + str("/") + str(folder) + str("/")
+    for f in file_list:
+        file_path = folder_path + f
+        f1 = open(file_path,'r')
+        for line in f1:
+            print line
+
+file_list = list_files('subtitles')
+process(file_list,'subtitles')
+
