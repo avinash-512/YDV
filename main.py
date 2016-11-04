9147c48ce4cb633357814e0c169fd0ed9dd928a6
avinah512 <abhinayavinash@iitkgp.ac.in>
2016-11-04 06:27:06 +0530

    Final Changes made

---
+++ main.py
@@ -0,0 +1,5 @@
+from preprocessing import pre_process
+
+
+pre_process('subtitles','subtitles-text')
+
--- preprocessing.py
+++ preprocessing.py
@@ -7,29 +7,47 @@
 
 import os
 import pycaption
-def list_files(folder):
-    current_path = os.getcwd()
-    print(current_path)
-    folder_path = str(current_path) + str("/") + str(folder) + str("/")
-    list_command = "ls " + folder_path
-    file_list = os.popen(list_command).read().split("\n")
-    file_list.remove('')
-    return file_list
 
-def process(file_list,folder):
+
+def pre_process(folder,output_folder):
+    #Initializing WebVTT Reader
     webtv_reader = pycaption.WebVTTReader()
     current_path = os.getcwd()
+    #Collecting file_names
+    folder_path = str(current_path) + str("/") + str(folder) + str("/")
+    list_command = "ls " + folder_path
+    print("Collecting subtitle files......")
+    file_list = os.popen(list_command).read().split("\n")
+    file_list.remove('')
     output_folder = os.getcwd() + str("/") + str(output_folder)
     folder_path = str(current_path) + str("/") + str(folder) + str("/")
+    #Converting files to srt format
     for f in file_list:
-        file_path = folder_path + str("/") + str(f)
-        print("converting "+f+"....")
-        output_path = outtput_folder + str("/") + str(f)
-        f1 = open(file_path,'r')
-        out = pycation.WebVTTWriter().write(f1).encode("utf-8")
-        fout = f.replace(".vtt",".srt")
+        if '.vtt' not in f:
+            continue
+        file_path = folder_path +  str(f)
+        print("converting " + f + "....")
+        output_path = output_folder + str("/") + str(f.replace('.vtt','.srt'))
+        f1 = open(file_path,'r').read()
+        n = webtv_reader.read(f1.decode('utf-8'))
+        out = pycaption.SRTWriter().write(n).encode("utf-8").split("\n")
+        fout = ''
+        for line in out:
+            if "-->" in line:
+                continue
+            if line == '\n':
+                continue
+            if line == '':
+                continue
+            try:
+                x = int(line)
+                continue
+            except:
+                fout += str(line)
+                fout += str("\n")
+        
         f2 = open(output_path,"w")
         f2.write(fout)
         f2.close()
-        f1.close()
+
 
